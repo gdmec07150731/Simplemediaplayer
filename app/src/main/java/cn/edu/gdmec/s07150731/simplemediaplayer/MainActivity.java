54bf2d32a8de3e1e45c4414c65cdb77b4fc9fd44
gdmec07150731 <907401314@qq.com>
2016-12-31 23:26:47 +0800

    补签

--- app/src/main/java/cn/edu/gdmec/s07150731/simplemediaplayer/MainActivity.java
+++ app/src/main/java/cn/edu/gdmec/s07150731/simplemediaplayer/MainActivity.java
@@ -1,8 +1,5 @@
 package cn.edu.gdmec.s07150731.simplemediaplayer;
 
-import android.content.Intent;
-import android.media.MediaPlayer;
-import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -10,90 +7,83 @@ import android.view.MenuItem;
 import android.widget.MediaController;
 import android.widget.Toast;
 import android.widget.VideoView;
-
-import java.io.IOException;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 
 public class MainActivity extends AppCompatActivity {
-
-    private static  final String TAG = "SimpleMediaPlayer";
+    private static final String TAG="SimpleMediaPlayer";
     private MediaPlayer mMediaPlayer;
     private String mPath;
     private VideoView mVideoView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Intent intent  = getIntent();
-        Uri uri = intent.getData();
-        if(uri!=null){
+        Intent intent = getIntent();
+        Uri uri =intent.getData();
+        if (uri !=null){
             mPath = uri.getPath();
             setTitle(mPath);
-            if(intent.getType().contains("audio")){
+            if (intent.getType().contains("audio")){
                 setContentView(android.R.layout.simple_list_item_1);
-                mMediaPlayer = new MediaPlayer();
-
-                try {
+                mMediaPlayer =new MediaPlayer();
+                try{
                     mMediaPlayer.setDataSource(mPath);
                     mMediaPlayer.prepare();
                     mMediaPlayer.start();
-                } catch (IOException e) {
+
+                }catch (Exception e) {
                     e.printStackTrace();
                 }
-
             }else if(intent.getType().contains("video")){
-                mVideoView = new VideoView(this);
+                mVideoView =new VideoView(this);
                 mVideoView.setVideoPath(mPath);
                 mVideoView.setMediaController(new MediaController(this));
-                mVideoView.start();
                 setContentView(mVideoView);
             }
         }
-
     }
 
-
     @Override
-    public boolean onCreateOptionsMenu(Menu menu){
+    public boolean onCreateOptionsMenu(Menu menu) {
         menu.add(0,1,0,"暂停");
         menu.add(0,2,0,"开始");
+
         return super.onCreateOptionsMenu(menu);
     }
 
-
-
     @Override
-    public boolean onOptionsItemSelected(MenuItem item){
-    switch (item.getItemId()){
-        case 1:
-            if(mMediaPlayer==null||!mMediaPlayer.isPlaying()){
-                Toast.makeText(this,"没有音乐文件,不需要暂停",Toast.LENGTH_SHORT);
-            }else{
-                mMediaPlayer.pause();
-            }
-        case 2:
-            if(mMediaPlayer==null){
-                Toast.makeText(this, "没有选中文件,请到文件夹中点击音乐文件后再播放", Toast.LENGTH_SHORT).show();
-            }else{
-                mMediaPlayer.start();
-            }
-            break;
-    }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case 1:
+                if (mMediaPlayer==null || !mMediaPlayer.isPlaying()){
+                    Toast.makeText(this,"没有印了文件，不需要暂停",
+                            Toast.LENGTH_SHORT).show();
+                }else {
+                    mMediaPlayer.pause();
+                }
+                break;
+            case 2:
+                if (mMediaPlayer==null){
+                    Toast.makeText(this,"没有选中音乐文件，青岛文件夹中点击音乐文件后再播放",
+                            Toast.LENGTH_SHORT).show();
+                }else{
+                    mMediaPlayer.start();
+                }
+                break;
+        }
         return super.onOptionsItemSelected(item);
     }
 
-
     @Override
-    protected void onDestroy(){
+    protected void onDestroy() {
         super.onDestroy();
-        if(mMediaPlayer!=null){
+        if (mMediaPlayer!=null){
             mMediaPlayer.stop();
         }
-        if (mVideoView!=null){
+        if(mVideoView!=null){
             mVideoView.stopPlayback();
         }
     }
-
-
 }
